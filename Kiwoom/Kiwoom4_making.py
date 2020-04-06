5228d7b26216741434795efe8dcc6d7ba344b162
monocsp <55018831+monocsp@users.noreply.github.com>
2020-04-07 08:10:58 +0900

    20200407

---
+++ Kiwoom/Kiwoom4_making.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtWidgets import *
+from tkinter import*
+import tkinter.messagebox
+from PyQt5.QAxContainer import *
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.kiwoom.dynamicCall("CommConnect()")
+
+        self.setWindowTitle("종목 코드")
+        self.setGeometry(300, 300, 500, 300)
+
+        btn1 = QPushButton("종목코드 얻기", self)
+        btn1.move(270, 10)
+        btn1.clicked.connect(self.btn1_clicked)
+
+        self.listWidget = QListWidget(self)
+        self.listWidget.setGeometry(10, 10, 250, 250)
+
+    def btn1_clicked(self):
+        ret = self.kiwoom.dynamicCall("GetCodeListByMarket(QString)", ["0"])
+        kospi_code_list = ret.split(';')
+        kospi_code_name_list = []
+
+        for x in kospi_code_list:
+            name = self.kiwoom.dynamicCall("GetMasterCodeName(QString)", [x])
+            kospi_code_name_list.append(x + " : " + name)
+
+        self.listWidget.addItems(kospi_code_name_list)
+        self.listWidget.itemDoubleClicked.connect(self.list_Clicked)
+    def list_Clicked(self):
+        print(self.listWidget.currentItem().text())
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    sys.exit(app.exec_())
